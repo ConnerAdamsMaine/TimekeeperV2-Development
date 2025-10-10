b3244b4f5750149d13689ef6f62daac7d8f86dde
Conner <Conneradamsmaine@gmail.com>
2025-10-09 21:01:21 -0400

    More fixing

--- API/__init__.py
+++
@@ -1,3 +0,0 @@
-from .app import create_app, run_api
-
-__all__ = ['create_app', 'run_api']
--- API/middleware/__init__.py
+++
@@ -1,3 +0,0 @@
-from auth import APIAuth, require_api_key, require_tier, APITier
-
-__all__ = ['APIAuth', 'require_api_key', 'require_tier', 'APITier']
